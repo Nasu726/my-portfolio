cddab204c8f8648722d45b02feee475847739b45
nasu <nasu@mma.club.uec.ac.jp>
2026-01-05 23:08:46 +0900

    fontFamilyを編集

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: [
+					"Helvetica Neue",
+					"Arial",
+					"Meiryo", 
+					"Hiragino Gothic",
+					"sans-serif",
+				],
+			},
+		},
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {

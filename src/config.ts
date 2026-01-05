954879ff1e568c6d5c36424d5d19545efa00b818
nasu <nasu@mma.club.uec.ac.jp>
2026-01-05 22:54:56 +0900

    デフォルトから日本語での文に変更

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// このファイルにはグローバルデータを置いてください。
+// `import` のキーワードを使うと自分のサイト内のどこからでもインポートできます。
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'なす | 情報学徒';
+export const SITE_DESCRIPTION = '日本の国立大学で情報工学を勉強している大学生のマイページです。AstroとCloudflareで構築しています。';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true

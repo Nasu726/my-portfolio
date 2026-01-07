a76a0bfcaf62b3a14503b2a975859f1fab288406
nasu <nasu@mma.club.uec.ac.jp>
2026-01-07 21:25:58 +0900

    ページタイトルと説明を変更

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // このファイルにはグローバルデータを置いてください。
 // `import` のキーワードを使うと自分のサイト内のどこからでもインポートできます。
 
-export const SITE_TITLE = 'なす | 情報工学を学んでいます';
-export const SITE_DESCRIPTION = '日本の国立大学で情報工学を勉強している大学生のマイページです。AstroとCloudflareで構築しています。';
+export const SITE_TITLE = 'なす | マイページ';
+export const SITE_DESCRIPTION = 'なすのマイページです。AstroとCloudflareで構築しています。';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true

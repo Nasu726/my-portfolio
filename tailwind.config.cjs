12c7741a07a351e84ebe78e80e1959ade7c3a392
nasu <nasu@mma.club.uec.ac.jp>
2026-01-07 16:55:19 +0900

    ダークテーマをdraculaに変更

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: ['class', '[data-theme="dracula"]'],
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
@@ -17,8 +18,8 @@ module.exports = {
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
 		// themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		themes: ["light", "dark"],
-		darkTheme: "dark", // name of one of the included themes for dark mode
+		themes: ["light", "dracula"],
+		darkTheme: "dracula", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	  }
 }

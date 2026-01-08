1180773a37783df3ed6283805e6d334b259cd389
nasu <nasu@mma.club.uec.ac.jp>
2026-01-08 13:16:16 +0900

    積読スキーマを追加

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,21 @@ const blogSchema = z.object({
     }).optional(),
 });
 
+const bookSchema = z.object({
+    title: z.string(),
+    authors: z.union([z.string(), z.array(z.string())]),
+    translators: z.union([z.string(), z.array(z.string())]).optional(),
+    publisher: z.string(),
+    pubDate: z.coerce.date(),
+    regDate: z.coerce.date(),
+    url: z.string().optional(),
+    heroImage: z.string().optional().transform((str) => {return str || "/post_img.webp"}),
+    badge: z.union([z.string(), z.array(z.string())]).optional(),
+    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
+        message: 'tags must be unique',
+    }).optional(),
+});
+
 const storeSchema = z.object({
     title: z.string(),
     description: z.string(),
@@ -41,15 +56,18 @@ const projectSchema = z.object({
 });
 
 export type BlogSchema = z.infer<typeof blogSchema>;
+export type BookSchema = z.infer<typeof bookSchema>;
 export type StoreSchema = z.infer<typeof storeSchema>;
 export type ProjectSchema = z.infer<typeof projectSchema>;
 
 const blogCollection = defineCollection({ schema: blogSchema });
+const bookCollection = defineCollection({ schema: bookSchema });
 const storeCollection = defineCollection({ schema: storeSchema });
 const projectCollection = defineCollection({ schema: projectSchema });
 
 export const collections = {
     'blog': blogCollection,
+    'book': bookCollection,
     'store': storeCollection,
     'projects': projectCollection,
 }

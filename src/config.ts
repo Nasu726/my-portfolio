690566f03634d734aa833c617b69668eeb1816d9
nasu <nasu@mma.club.uec.ac.jp>
2026-01-26 11:50:06 +0900

    検索時に出てくる説明文を修正。

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // `import` のキーワードを使うと自分のサイト内のどこからでもインポートできます。
 
 export const SITE_TITLE = 'なす | マイページ';
-export const SITE_DESCRIPTION = 'なすのマイページです。AstroとCloudflareで構築しています。';
+export const SITE_DESCRIPTION = 'なすのマイページです。プロフィール、今までに作成したプロジェクトとブログ、本棚を公開しています。';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true

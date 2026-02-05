0d19d5d677f2c28da7955faa7806ed73cafc98b9
nasu <168271934+Nasu726@users.noreply.github.com>
2026-02-05 20:00:42 +0900

    astro.config.mjs を更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mypage.nasu.uk',
+  site: 'https://nasu.uk',
   integrations: [mdx(), sitemap(), tailwind()],
   image: {
     domains: ["www.hanmoto.com", "assets.nasu.uk"]

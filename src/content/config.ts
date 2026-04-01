906c9a4d9efa263783d493234324a8dae63cd655
nasu <nasu@mma.club.uec.ac.jp>
2026-04-02 01:06:28 +0900

    推定読了時間を表示するように改善

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,6 +15,7 @@ const blogSchema = z.object({
     tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
         message: 'tags must be unique',
     }).optional(),
+    readingTime: z.number(),
 });
 
 const bookSchema = z.object({

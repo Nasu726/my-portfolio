4f220f6095f9274a94ab870a704f51a484aea02c
nasu <nasu@mma.club.uec.ac.jp>
2026-04-01 23:47:34 +0900

    mermaid記法、TeX記法に対応。コードブロックにコピーボタンを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,22 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import mermaid from 'astro-mermaid';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nasu.uk',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mermaid(), mdx(), sitemap(), tailwind()],
   image: {
     domains: ["www.hanmoto.com", "assets.nasu.uk"]
+  },
+  markdown: {
+    shikiConfig: {
+      theme: "monokai",
+    },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 });
